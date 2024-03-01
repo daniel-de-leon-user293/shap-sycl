22c2cad7d62e45984e42e0afbf3688e7b7df89d6
Daniel Deleon <daniel.de.leon@intel.com>
2024-03-01 15:47:00 -0800

    seemd to compile with icpx but GPUTree errors

--- setup.py
+++ setup.py
@@ -9,6 +9,8 @@ from packaging.version import Version, parse
 from setuptools import Extension, setup
 
 _BUILD_ATTEMPTS = 0
+os.environ['CC']='icpx'
+os.environ['CXX']='icpx'
 
 # This is copied from @robbuckley's fix for Panda's
 # For mac, ensure extensions are built for macos 10.9 when compiling on a
@@ -65,6 +67,18 @@ def get_cuda_path():
 
     return cuda_home, nvcc
 
+def get_intel_path():
+    """Return a tuple with (base_intel_directory, full_path_to_icpx_compiler)"""
+    # eventually remove hard-code paths
+    relative_path_to_icpx = "compiler/latest/linux/bin/icpx"
+    if "ONEAPI_ROOT" in os.environ:
+        oneapi_root = os.environ["ONEAPI_ROOT"]
+        icpx = os.path.join(oneapi_root, relative_path_to_icpx)
+        return oneapi_root, icpx
+    else:
+        print('Failed to find OneAPI root. Be sure setvars.sh has been executed \
+              in your shell and ONEAPI_ROOT exists in your environment.')
+        return None
 
 def compile_cuda_module(host_args):
     libname = '_cext_gpu.lib' if sys.platform == 'win32' else 'lib_cext_gpu.a'
@@ -95,8 +109,35 @@ def compile_cuda_module(host_args):
     subprocess.run([nvcc] + nvcc_command.split(' '), check=True)
     return 'build', '_cext_gpu'
 
+def compile_sycl_module(host_args, icpx):
+    libname = '_cext_gpu.lib' if sys.platform == 'win32' else 'lib_cext_gpu.a'
+    lib_out = 'build/' + libname
+    if not os.path.exists('build/'):
+        os.makedirs('build/')
+    
+    print("ICPX ==> ", icpx)
+    # Do have any arch_flags to set?
+    '''
+    arch_flags = (
+        "-arch=sm_37 "
+        "-gencode=arch=compute_37,code=sm_37 "
+        "-gencode=arch=compute_70,code=sm_70 "
+        "-gencode=arch=compute_75,code=sm_75 "
+        "-gencode=arch=compute_75,code=compute_75"
+    )
+    '''
+    icpx_command = (
+        f"shap/cext/dpct_output/_cext_gpu.dp.cpp -c -o {lib_out} "
+        f"-I {sysconfig.get_path('include')} "
+        #f"-stdlib=c++14 "
+        f"{' '.join(host_args)}"
+    )
+    print([icpx] + icpx_command.split(' '))
+    subprocess.run([icpx] + icpx_command.split(' '), check=True )
+    return 'build', '_cext_gpu'
 
-def run_setup(*, with_binary, with_cuda):
+
+def run_setup(*, with_binary, with_cuda, with_sycl):
     ext_modules = []
     if with_binary:
         compile_args = []
@@ -105,6 +146,7 @@ def run_setup(*, with_binary, with_cuda):
         if sys.platform == 'win32':
             compile_args.append('/MD')
 
+        compile_args.append('-fsycl')
         ext_modules.append(
             Extension('shap._cext', sources=['shap/cext/_cext.cc'],
                       include_dirs=[np.get_include()],
@@ -131,18 +173,39 @@ def run_setup(*, with_binary, with_cuda):
         except Exception as e:
             raise Exception("Error building cuda module: " + repr(e)) from e
 
+    if with_sycl:
+        try:
+            intel_home, icpx = get_intel_path()
+            compile_args.append('-fPIC')
+            lib_dir, lib = compile_sycl_module(compile_args, icpx)
+            intelart_path = os.path.join(intel_home, 'compiler/latest/linux/bin/intel64') 
+            ext_modules.append(
+                #Extension('shap.dpct_output._cext_gpu', sources=['shap/cext/_cext_gpu.cc'],
+                Extension('shap._cext_gpu', sources=['shap/cext/_cext_gpu.cc'],
+                          extra_compile_args=compile_args,
+                          include_dirs=[np.get_include()],
+                          library_dirs=[lib_dir, intelart_path],
+                          libraries=[lib],
+                          depends=['shap/cext/dpct_output/_cext_gpu.dp.cpp',
+                                   'shap/cext/dpct_output/gpu_treeshap.h',
+                                   'setup.py'])
+            )
+        except Exception as e:
+            raise Exception("Error building sycl module: " + repr(e)) from e
+
+
     setup(ext_modules=ext_modules)
 
 
-def try_run_setup(*, with_binary, with_cuda):
+def try_run_setup(*, with_binary, with_cuda, with_sycl):
     """ Fails gracefully when various install steps don't work.
     """
     global _BUILD_ATTEMPTS
     _BUILD_ATTEMPTS += 1
 
     try:
-        print(f"Attempting to build SHAP: {with_binary=}, {with_cuda=} (Attempt {_BUILD_ATTEMPTS})")
-        run_setup(with_binary=with_binary, with_cuda=with_cuda)
+        print(f"Attempting to build SHAP: {with_binary=}, {with_cuda=}, {with_sycl=} (Attempt {_BUILD_ATTEMPTS})")
+        run_setup(with_binary=with_binary, with_cuda=with_cuda, with_sycl=with_sycl)
     except Exception as e:
         print("Exception occurred during setup,", str(e))
 
@@ -154,13 +217,17 @@ def try_run_setup(*, with_binary, with_cuda):
             with_binary = False
             print("WARNING: The C extension could not be compiled, sklearn tree models not supported.")
             print("Retrying SHAP build without binary extension...")
+        elif with_sycl:
+            with_sycl = False
+            print("WARNING: Could not compile SYCL implementation of GPUTree.")
+            print("Retrying SHAP build without SYCL extensions...")
         else:
             print("ERROR: Failed to build!")
             raise
 
-        try_run_setup(with_binary=with_binary, with_cuda=with_cuda)
+        try_run_setup(with_binary=with_binary, with_cuda=with_cuda, with_sycl=with_sycl)
 
 
 # we seem to need this import guard for appveyor
 if __name__ == "__main__":
-    try_run_setup(with_binary=True, with_cuda=True)
+    try_run_setup(with_binary=True, with_cuda=True, with_sycl=True)
